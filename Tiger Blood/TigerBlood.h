abd475fe55efe20b1edecc7b90f520aa64ef6857
travismcox <travis.m.cox@mac.com>
2018-03-26 17:04:19 -0500

    Added Code from wiki, renamed Move to Move_TigerBlood

--- Tiger Blood/TigerBlood.h
+++ Tiger Blood/TigerBlood.h
@@ -9,5 +9,33 @@
 #ifndef TigerBlood_h
 #define TigerBlood_h
 
+enum Color_t {RED, BLUE};
+
+struct Point_t{
+    int row, col;
+};
+
+struct Token_t{
+    Color color;
+    Point location;
+};
+
+struct Move_t{
+    Token_t token;
+    Point_t destination;
+};
+
+Move_t  Move_TigerBlood(vector<Token_t>, Color_t turn);
+
+/*
+ * Each group will implement the Move function and
+ * submit their code using the Upload Site.
+ *
+ * You should name your move function: Move_GroupName.
+ *
+ * This will create a function prototype something like:
+ *
+ *  Move_t Move_GroupName(vector<Token_t>, Color_t turn);
+ */
 
 #endif /* TigerBlood_h */
